c506e14fc856c56c54fb3ade62b133ed3f2c3749
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-12-15 17:55:22 +0300

    add login ande regestr pages

--- app/GlobalProvider.jsx
+++ app/GlobalProvider.jsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
+import { SessionProvider } from "next-auth/react";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function GlobalProvider({ children }) {
-  return <CartProvider>{children}</CartProvider>;
+  return (
+    <>
+      <ToastContainer position="bottom-right" />
+      <AuthProvider>
+        <CartProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </CartProvider>
+      </AuthProvider>
+    </>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
     DB_URI: "mongodb+srv://yasharnajafi6846:sacenthandes@cluster0.u3zadxk.mongodb.net",
+    NEXTAUTH_SECRET: "codingwithabbas",
   },
   images: {
     domains: ["res.cloudinary.com"],
